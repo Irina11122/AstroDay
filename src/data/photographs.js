46b64a6b37772e0ad08b14d1cb04ea45106f8b29
Irina <irina.bojkovska.ib@gmail.com>
2026-08-18 09:30:11 -0700

    update gallery

--- src/data/photographs.js
+++ src/data/photographs.js
@@ -1,65 +1,235 @@
 export const photographs = [
   {
     id: 1,
-    title: 'Afghan Girl',
-    photographer: 'Steve McCurry',
-    year: 1984,
-    category: 'Portrait',
-    image: 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce',
+    title: 'View from the Window at Le Gras',
+    photographer: 'Joseph Nicéphore Niépce',
+    year: 1827,
+    category: 'History',
+    image:
+      'https://smarthistory.org/wp-content/uploads/2022/06/niepce-heliograph-1024-copy.jpg',
     description:
-      'One of the most recognizable portraits in the history of photography.',
+      'The oldest surviving photograph, created by Nicéphore Niépce around 1826–1827 using the heliography process.',
   },
 
   {
     id: 2,
-    title: 'Earthrise',
-    photographer: 'William Anders / NASA',
-    year: 1968,
+    title: 'The Moon',
+    photographer: 'John Whipple & William Cranch Bond',
+    year: 1851,
     category: 'Space',
-    image: 'https://images.unsplash.com/photo-1446776877081-d282a0f896e2',
+    image: 'https://cdn.loc.gov/service/pnp/ppmsca/22100/22196v.jpg',
     description:
-      'An extraordinary view of Earth rising above the lunar horizon.',
+      'One of the earliest successful photographs of the Moon, created at the Harvard College Observatory in 1851.',
   },
 
   {
     id: 3,
-    title: 'Migrant Mother',
-    photographer: 'Dorothea Lange',
-    year: 1936,
-    category: 'Documentary',
-    image: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91',
+    title: 'Mt. Everest',
+    photographer: 'Unknown / 1953 Everest Expedition',
+    year: 1953,
+    category: 'Adventure',
+    image:
+      'https://newsroom-content.rolex.com/-/media/project/rolex/newsroom/rolex/rolex-newsroom-int/world-of-rolex/perpetual-planet/rolex-and-exploration/everest-ascent-anniversary/visuals/b_everest_1953.jpg?imwidth=740&impolicy=grid-1&imdensity=2',
     description:
-      'An iconic image representing hardship during the Great Depression.',
+      'Edmund Hillary and Tenzing Norgay became the first confirmed climbers to reach the summit of Mount Everest on 29 May 1953.',
   },
 
   {
     id: 4,
-    title: 'Tank Man',
-    photographer: 'Jeff Widener',
-    year: 1989,
-    category: 'History',
-    image: 'https://images.unsplash.com/photo-1521292270410-a8c4d716d518',
+    title: 'John Quincy Adams',
+    photographer: 'Philip Haas',
+    year: 1843,
+    category: 'Portrait',
+    image: 'https://cdn.loc.gov/service/pnp/pga/06900/06984v.jpg',
     description:
-      'An unknown man standing in front of tanks became a symbol of resistance.',
+      'The earliest known photograph of a U.S. president, showing John Quincy Adams in a daguerreotype made by Philip Haas in 1843.',
   },
 
   {
     id: 5,
-    title: 'Lunch Atop a Skyscraper',
-    photographer: 'Unknown',
-    year: 1932,
-    category: 'History',
-    image: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df',
-    description: 'Construction workers sitting high above New York City.',
+    title: 'The Valley of the Shadow of Death',
+    photographer: 'Roger Fenton',
+    year: 1855,
+    category: 'War Photography',
+    image:
+      'https://www.meisterdrucke.de/kunstwerke/1260px/Roger_Fenton_-_The_Valley_of_the_Shadow_of_Death_-_%28MeisterDrucke-8485%29.jpg',
+    description:
+      'One of the earliest and most iconic war photographs, showing a road scattered with cannonballs during the Crimean War.',
   },
 
   {
     id: 6,
-    title: 'V-J Day in Times Square',
-    photographer: 'Alfred Eisenstaedt',
-    year: 1945,
-    category: 'Culture',
-    image: 'https://images.unsplash.com/photo-1529333166437-7750a6dd5a70',
-    description: 'A spontaneous kiss celebrating the end of World War II.',
+    title: 'First Photograph of the Sun',
+    photographer: 'Hippolyte Fizeau & Léon Foucault',
+    year: 1845,
+    category: 'Astronomy',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/8/83/Fizeau_Foucault-First_Photo_of_Sun_1845.jpg',
+    description:
+      'The first successful photograph of the Sun, captured in Paris on 2 April 1845 using the daguerreotype process. The image reveals several sunspots.',
+  },
+
+  {
+    id: 7,
+    title: 'First Underwater Photograph',
+    photographer: 'Louis Boutan',
+    year: 1893,
+    category: 'Underwater Photography',
+    image:
+      'https://www.earthlymission.com/wp-content/uploads/2020/03/first_underwater_portrait.jpg',
+    description:
+      'One of the earliest underwater photographs, created by Louis Boutan during his pioneering experiments in Banyuls-sur-Mer, France.',
+  },
+
+  {
+    id: 8,
+    title: 'First Solar Eclipse Photograph',
+    photographer: 'Johann Julius Friedrich Berkowski',
+    year: 1851,
+    category: 'Astronomy',
+    image:
+      'https://bilder.deutschlandfunk.de/FI/LE/_e/48/FILE_e481282a38a74b5b8bff020fcbf9e56e/20160728a-daguerreoty-sonn-jpg-100-768x432.jpg',
+    description:
+      'The first known photograph of a total solar eclipse, captured by Johann Julius Friedrich Berkowski on 28 July 1851 using the daguerreotype process.',
+  },
+
+  {
+    id: 9,
+    title: 'First Image of a Black Hole',
+    photographer: 'Event Horizon Telescope Collaboration',
+    year: 2019,
+    category: 'Astronomy',
+    image: 'https://astrobites.org/wp-content/uploads/2025/06/m87.jpg',
+    description:
+      'The first-ever image of a black hole, revealing the shadow of the supermassive black hole M87* at the center of the Messier 87 galaxy.',
+  },
+
+  {
+    id: 10,
+    title: 'Curiosity on Mars',
+    photographer: 'NASA / JPL-Caltech',
+    year: 2012,
+    category: 'Space',
+    image:
+      'https://th.bing.com/th/id/R.83b4866106833497cef33a983b9d408b?rik=7KFPscrrmYAGFg&riu=http%3a%2f%2fredplanet.asu.edu%2fwp-content%2fuploads%2f2015%2f10%2fPIA19920_modest.jpg&ehk=UjBAmibLRp4FKFuYSnBRZGbdqD8E9om7U8dxH4qvy4k%3d&risl=&pid=ImgRaw&r=0',
+    description:
+      'A self-portrait of NASA’s Curiosity rover on Mars, showing the vehicle exploring Gale Crater in 2012.',
+  },
+
+  {
+    id: 11,
+    title: 'Pluto',
+    photographer: 'NASA / New Horizons',
+    year: 2015,
+    category: 'Space',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Nh-pluto-in-true-color_2x_JPEG-edit.jpg/1280px-Nh-pluto-in-true-color_2x_JPEG-edit.jpg',
+    description:
+      'The first detailed close-up view of Pluto, captured by NASA’s New Horizons spacecraft in 2015.',
+  },
+
+  {
+    id: 12,
+    title: 'Earth from Space',
+    photographer: 'Apollo 17 / NASA',
+    year: 1972,
+    category: 'Earth',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg',
+    description:
+      'The iconic Blue Marble photograph of Earth, captured by the Apollo 17 crew during their journey to the Moon in 1972.',
+  },
+
+  {
+    id: 13,
+    title: 'Webb’s First Deep Field',
+    photographer: 'NASA / ESA / CSA / STScI',
+    year: 2022,
+    category: 'Deep Space',
+    image: 'https://stsci-opo.org/STScI-01G7JGJ4Z4Q4J5H3YJQY8Z8M3C.png',
+    description:
+      'The first deep-field image from the James Webb Space Telescope, revealing thousands of distant galaxies.',
+  },
+
+  {
+    id: 14,
+    title: 'Pillars of Creation',
+    photographer: 'NASA / ESA / CSA / STScI',
+    year: 2022,
+    category: 'Nebula',
+    image:
+      'https://science.nasa.gov/wp-content/uploads/2023/09/pillars-of-creation.jpg',
+    description:
+      'A spectacular James Webb Space Telescope view of the Pillars of Creation in the Eagle Nebula.',
+  },
+
+  {
+    id: 15,
+    title: 'The Falling Man',
+    photographer: 'Richard Drew',
+    year: 2001,
+    category: 'Documentary',
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/6e/Falling_Man.jpg',
+    description:
+      'A powerful and controversial photograph taken during the September 11 attacks in New York City.',
+  },
+
+  {
+    id: 16,
+    title: 'The Situation Room',
+    photographer: 'Pete Souza',
+    year: 2011,
+    category: 'Documentary',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5e/President_Barack_Obama_and_members_of_the_national_security_team_receive_an_update_on_the_mission_against_Osama_bin_Laden.jpg',
+    description:
+      'A historic photograph showing President Barack Obama and his national security team monitoring the operation against Osama bin Laden.',
+  },
+
+  {
+    id: 17,
+    title: 'Barack Obama – Hope',
+    photographer: 'Shepard Fairey',
+    year: 2008,
+    category: 'Art',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/8/87/Barack_Obama_Hope_poster.jpg',
+    description:
+      'An iconic image that became one of the most recognizable political artworks of the 21st century.',
+  },
+
+  {
+    id: 18,
+    title: 'Steve Jobs',
+    photographer: 'Albert Watson',
+    year: 2006,
+    category: 'Portrait',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/3/31/Steve_Jobs_Headshot_2010-CROP.jpg',
+    description:
+      'A striking portrait of Apple co-founder Steve Jobs, photographed by Albert Watson.',
+  },
+
+  {
+    id: 19,
+    title: 'Felix Baumgartner',
+    photographer: 'Red Bull',
+    year: 2012,
+    category: 'Adventure',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5c/Felix_Baumgartner_2012.jpg',
+    description:
+      'Felix Baumgartner during his record-breaking free fall from the stratosphere in 2012.',
+  },
+
+  {
+    id: 20,
+    title: 'Perseverance on Mars',
+    photographer: 'NASA / JPL-Caltech',
+    year: 2021,
+    category: 'Science',
+    image:
+      'https://mars.nasa.gov/system/resources/detail_files/25508_PIA24431-web.jpg',
+    description: 'NASA’s Perseverance rover exploring Jezero Crater on Mars.',
   },
 ];
