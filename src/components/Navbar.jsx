16b35c0a32af174dc923f29b613127e94b286bab
Irina <irina.bojkovska.ib@gmail.com>
2026-08-20 00:13:27 -0700

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ export default function Navbar() {
 
   return (
     <header className="navbar">
-      {/* PRAXIS LOGO */}
       <Link to="/" className="navbar-praxis">
         <img
           src="/praxis-logo.png"
@@ -19,24 +18,33 @@ export default function Navbar() {
         />
       </Link>
 
-      {/* ASTROGRAVIA */}
       <div className="navbar-brand">
         <Link to="/" className="astrogravia-logo">
-          ASTROGRAVIA
+          ASTROWORLD FOR PRAXIS
         </Link>
       </div>
 
-      {/* DESKTOP NAVIGATION */}
       <nav className="navbar__nav">
-        <Link to="/">HOME</Link>
-        <Link to="/photo">PHOTOGRAPHY</Link>
-        <Link to="/universe">UNIVERSE</Link>
-        <Link to="/gallery">GALLERY</Link>
-        <Link to="/contact">CONTACT</Link>
-        <Link to="/club-serres-unesco">UNESCO</Link>
+        <Link to="/" onClick={closeMenu}>
+          HOME
+        </Link>
+        <Link to="/photo" onClick={closeMenu}>
+          PHOTOGRAPHY
+        </Link>
+        <Link to="/universe" onClick={closeMenu}>
+          UNIVERSE
+        </Link>
+        <Link to="/gallery" onClick={closeMenu}>
+          GALLERY
+        </Link>
+        <Link to="/contact" onClick={closeMenu}>
+          CONTACT
+        </Link>
+        <Link to="/club-serres-unesco" onClick={closeMenu}>
+          UNESCO
+        </Link>
       </nav>
 
-      {/* EMAIL */}
       <div className="navbar-partners">
         <a
           href="https://mail.google.com/mail/?view=cm&fs=1&to=praxis@praxisgreece.com"
@@ -48,18 +56,17 @@ export default function Navbar() {
         </a>
       </div>
 
-      {/* MOBILE MENU BUTTON */}
       <button
         className={`mobile-menu-button ${menuOpen ? 'active' : ''}`}
         onClick={() => setMenuOpen(!menuOpen)}
-        aria-label="Toggle navigation menu"
+        aria-label="Open navigation menu"
+        aria-expanded={menuOpen}
       >
         <span></span>
         <span></span>
         <span></span>
       </button>
 
-      {/* MOBILE MENU */}
       <nav className={`mobile-nav ${menuOpen ? 'open' : ''}`}>
         <Link to="/" onClick={closeMenu}>
           HOME
